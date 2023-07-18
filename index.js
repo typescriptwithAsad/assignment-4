99cc82fb276aabc3c5d8ae4a44a0f964d12d8782
asad <139746796+typescriptwithAsad@users.noreply.github.com>
2023-07-18 17:50:15 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+// Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+// Your output should look something like the following, including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+// Quide Azam Muhammad Ali Jinnah said,"there is no any strenght in the world who destroye pakistan"
+console.log('Quide Azam Muhammad Ali Jinnah said,"There is no power in the world that can the destroye existence of pakistan"');
